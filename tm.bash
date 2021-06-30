6064585d919ae9c3c2971201ae09884b7c8e77a2
BHH007 <84727347+BHH007@users.noreply.github.com>
2021-06-30 20:21:06 +0600

    Create tm.bash

---
+++ tm.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+ {
+echo -e "${biru}                                     
+ ####### #     #       #     #
+    #    ##   ##       ##    #  ####   ####  #####
+    #    # # # #       # #   # #    # #    # #    #
+    #    #  #  # ##### #  #  # #    # #    # #####
+    #    #     #       #   # # #    # #    # #    #
+    #    #     #       #    ## #    # #    # #    #
+    #    #     #       #     #  ####   ####  #####
+
+${biru2}Coded by BH007${n}\n"
+}
+echo ""
+echo -e  $g "Please  Wait  A few Minutes...... "
+echo ""
+apt update
+apt upgrade 
+pkg update 
+pkg upgrade
+apt install wget
+pkg install python
+pkg install python2
+pkg install nano
+pkg install php
+pkg install cmatrix
+pkg install curl
+pkg install host
+pkg install perl 
+pkg install nmap 
+pkg install havij
+pkg install zip
+pkg install chroot
+pkg install openssh
+pkg install golang
+pkg install wireshark
+apt install mathomatic
+pip2 install requests
+pip2 install mechanize
+pkg install bash
+pkg install ruby | lolcat
